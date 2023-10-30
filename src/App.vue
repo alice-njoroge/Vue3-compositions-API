a3b10390797989506022db8f18fbe7517c2bbe0a
Alice Wanjiku <alicenjoroge707@gmail.com>
2023-10-30 15:55:21 +0300

    introudction to Vue 3 - compositions API

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<script setup>
+import {ref} from "vue";
+
+const header = ref('Shopping Portal');
+const newItem = ref('');
+const highPriority = ref(false);
+
+const items = ref([
+  {name: 'pens', id: 1},
+  {name: 'laptops', id: 2},
+  {name: 'books', id: 3},
+])
+
+
+</script>
+
+<template>
+
+  <h1>{{ header }}</h1>
+  <ul v-for="item in items" :key="item.id" >
+    <li> {{item.name}} </li>
+  </ul>
+  <hr>
+
+  <h2>Add New </h2>
+  <input v-model.trim="newItem" placeholder="add a new item" >
+  <input v-model="highPriority" type="checkbox">
+  <br/>
+  <button @click="items.push({id:items.length + 1, name: newItem})" class="btn btn-primary"> Add</button>
+</template>
+
+<style scoped>
+
+</style>
