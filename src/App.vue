72f20c10403b9b238fb2c8d970f0fa3d6ccf8732
Alice Wanjiku <alicenjoroge707@gmail.com>
2023-11-07 16:30:42 +0300

    exploring the setup method in compositions API

--- src/App.vue
+++ src/App.vue
@@ -1,66 +1,22 @@
-<script setup>
-import {computed, reactive, ref} from "vue";
+<script>
+export default {
+  setup() {
+    const name = "Cheesy Dribbler";
+    const placeOrder = () => alert('your order has been placed!')
 
-const header = ref('Shopping Portal');
-const newItem = ref('');
-const editing = ref(false);
-const highPriority = ref(false);
+    return {name, placeOrder}
 
-
-const items = reactive([
-  {name: 'pens', highPriority: false, id: 1},
-  {name: 'laptops', highPriority: false, id: 2},
-  {name: 'books', highPriority: true, id: 3},
-])
-
-const addItem = () => {
-  items.push({id: items.length + 1, name: newItem.value});
-  newItem.value = '';
+  }
 }
-
-const doEdit = (e)=>{
-  editing.value = e;
-  newItem.value = '';
-}
-
-const charCount = computed(()=>{
-  return newItem.value.length;
-})
-const reversedItems = computed(()=>{
-  return [...items].reverse()
-})
-
 
 </script>
 
 <template>
 
   <div class="container">
-    <div class="card m-3">
-      <div class=" mx-3 card-title">
-        <h2>{{ header }}</h2>
-        <button @click="doEdit(false)" class=" mx-2 btn btn-secondary">cancel</button>
-        <button @click="doEdit(true)" class="btn btn-primary" >Add Items</button>
-      </div>
+    <h1>{{name}}</h1>
+    <button @click="placeOrder"> Place order</button>
 
-      <div class="card-body">
-        <ul v-if="items.length" v-for="({item, name, highPriority, id}, index ) in reversedItems" :key="id">
-          <li :class="{ priority: highPriority }" > {{ name }}</li>
-        </ul>
-
-        <p v-else> Sorry, Nothing to see here </p>
-        <hr>
-        <form v-if="editing" @submit.prevent="addItem">
-          <h3>Add New </h3>
-          <input v-model.trim="newItem" @keyup.enter="addItem"
-                 placeholder="add a new item">
-          <input class="m-2" v-model="highPriority" type="checkbox">
-          <button :disabled="newItem.length < 3" type="submit" class="btn btn-primary m-2"> Add</button>
-          <p>{{charCount}}/200</p>
-        </form>
-      </div>
-
-    </div>
   </div>
 
 
@@ -69,7 +25,7 @@ const reversedItems = computed(()=>{
 <style scoped>
 
 .priority {
-  color: red ;
+  color: red;
 }
 
 </style>
