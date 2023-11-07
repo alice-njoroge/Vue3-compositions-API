94d5fc2852d608d71570a14303034ffc711322d6
Alice Wanjiku <alicenjoroge707@gmail.com>
2023-11-07 16:57:37 +0300

    using context - emit in setup method

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,13 @@
 <script>
+import YummyMeal from "@/components/YummyMeal.vue";
 export default {
+  components: {YummyMeal},
   setup() {
     const name = "Cheesy Dribbler";
     const placeOrder = () => alert('your order has been placed!')
+    const addItem = (item) => alert(`adding ${item} to cart`)
 
-    return {name, placeOrder}
+    return {name, placeOrder, addItem}
 
   }
 }
@@ -15,7 +18,8 @@ export default {
 
   <div class="container">
     <h1>{{name}}</h1>
-    <button @click="placeOrder"> Place order</button>
+    <button @click="placeOrder" > Place order</button>
+    <YummyMeal name="Burger" @addToCart="addItem" price="5"/>
 
   </div>
 
---
+++ src/components/YummyMeal.vue
@@ -0,0 +1,30 @@
+<script>
+export default {
+  props: {
+    name: String,
+    price: Number,
+  },
+  setup(props, {emit}) {
+    const addToCart = ()=> emit('addToCart', props.name);
+
+    return {addToCart}
+
+  }
+
+}
+
+</script>
+
+<template>
+
+<div>
+  <strong> 🍔- {{ name}}</strong>
+  {{price}}
+  <button @click="addToCart">Add to Cart</button>
+</div>
+
+</template>
+
+<style scoped>
+
+</style>
